7adf9a43f90bfd393c9801c1b2f53c251b0124f8
Thomas Dau <ThomasQDau@gmail.com>
2019-04-22 17:26:53 -0700

    m

---
+++ config/orm.js
@@ -0,0 +1,7 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+    //SOMETHING HERE!
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO burgers (burger_name) VALUES ("BLT Burger"), ("Jalepenos Burger"), ("Grilled Chicken");
